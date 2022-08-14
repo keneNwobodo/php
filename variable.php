5eecf93fe0d2c167642c33f9f4d32620c9889e9e
kenenwobodo <nwobodokenechukwu2@gmail.com>
2022-08-14 23:44:34 +0100

    played around with variables n php

---
+++ variable.php
@@ -0,0 +1,27 @@
+<?php
+// integers
+
+$int_num = 122456;
+
+// oubles
+$int_double = 22.56;
+echo  $int_double;
+
+# booleans
+if (true) 
+echo "My name is kenechukuw";
+else
+echo 'My name is not Kenechukwu';
+
+# NULL
+$null_val = null;
+echo $null_val;
+
+# strings
+$my_name = "Kenechukwu";
+echo $my_name;
+
+
+
+
+?>
